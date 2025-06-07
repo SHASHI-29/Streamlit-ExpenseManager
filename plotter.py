428af7695a269c09c69375031a56d3cfa549e0b3
Bandari Shasheendra <119602312+SHASHI-29@users.noreply.github.com>
2025-06-07 15:03:59 +0530

    Update plotter.py

--- plotter.py
+++ plotter.py
@@ -13,5 +13,6 @@ def plot_bar_chart(by_category):
     fig, ax = plt.subplots()
     sns.barplot(x="Category", y="Amount", data=by_category, ax=ax)
     ax.set_title("Total Amount per Category")
-    ax.bar_label(ax.containers[0])
+    if ax.containers:
+        ax.bar_label(ax.containers[0])
     return fig
